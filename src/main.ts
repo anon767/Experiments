58f2fa4c7fb4f78cdfcd52c756c3af61b4884abe
ossprey-bench <noreply@local>
2026-06-30 23:03:31 +0000

    Fix

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideToastr } from 'ngx-toastr';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideBrowserGlobalErrorListeners(), provideToastr()],
-}).catch(err => console.error(err));
